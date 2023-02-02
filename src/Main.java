27d47b9bee9c4e6aa3bbd735df2f045fc6c4390a
JohnLudloww <joh2379087@maricopa.edu>
2023-02-02 14:43:47 -0700

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //testing random array stuff.
+        int[] arr;
+        int arr2[];
+
+        arr = new int[10];
+        System.out.println(arr.length);
+        System.out.println(arr[0]);
+
+
+        double[] arr3 = new double [10];
+        System.out.println(arr3[0]);
+
+        boolean[] arr4 = new boolean[10];
+        System.out.println(arr4[0]);
+
+        //array of arrays
+        int[][] hello = new int [10][0];
+        int[] arr5 = {1,2,3};
+        System.out.println(hello[0]);
+        System.out.println(Arrays.toString(arr5));
+
+
+
+
     }
 }
---
+++ src/operator.java
@@ -0,0 +1,13 @@
+public class operator {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+
+
+}
